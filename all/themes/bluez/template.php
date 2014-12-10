6f4d329e96cd78489aed2ae61c6c672a67db17e6
Samuel Silva <samucasilva@gmail.com>
2014-12-10 00:50:21 -0200

    Nova atualização

--- all/themes/bluez/template.php
+++ all/themes/bluez/template.php
@@ -119,6 +119,10 @@ function bluez_theme($existing, $type, $theme, $path) {
     'processos_basico' => $base + array(
       'template' => 'processos-basico',
     ),
+    'listagem_basica' => $base + array(
+      'template' => 'listagem-basica',
+    ),
+    
   );
 }
 
@@ -127,9 +131,19 @@ function bluez_theme($existing, $type, $theme, $path) {
  */
 function bluez_preprocess_processos_basico(&$variables) {
   /* Add or modify your variables */
-  dpr($variables);
+  //dpr($variables);
+  //echo "testes";
+/*  $base_path = 'http://localhost/labsoft/';
+  $output = '<div id="main"><div id="post-content">';
+  
+  $output .= '<a class="menu-button" title="Link para processos correntes" href="'.$base_path.'processos/processo_corrente"><img class="icon" src="'.$base_path.'/'.drupal_get_path('theme', $GLOBALS['theme']).'/images/pencil.png"><br/>Corrente</a>';
+  $output .= '<a class="menu-button" title="Link para processos arquivados" href="'.$base_path.'processos/processo_arquivado"><img class="icon" src="'.$base_path.'/'.drupal_get_path('theme', $GLOBALS['theme']).'/images/search.png"><br/>Arquivado</a>';
+ 	$output = '<a class="menu-button" title="Cadastre os dados dos clientes nesta parte do sistema" href="<?php echo $base_path; ?>processos"><img class="icon" src="<?php echo $base_path.'/'.drupal_get_path('theme', $GLOBALS['theme']); ?>/images/paper.png"><br/>Processos</a>'.
+//	$output = '<a class="menu-button" title="Cadastre os dados dos clientes nesta parte do sistema" href="<?php echo $base_path; ?>"><img class="icon" src="<?php echo $base_path.'/'.drupal_get_path('theme', $GLOBALS['theme']); ?>/images/search.png"><br/>Pesquisar</a>';
+  $output .='</div></div>';
+  print $output;
+*/  
 }
 
-
-
-
+function bluez_preprocess_listagem_basica(&$variables) {
+}
--- all/themes/bluez/templates/page--node--1.tpl.php
+++ all/themes/bluez/templates/page--node--1.tpl.php
@@ -64,20 +64,20 @@
     <div id="post-content">
         <a class="menu-button" title="Link para a lista de processos" href="<?php echo $base_path; ?>cadastro/cliente"><img class="icon" src="<?php echo $base_path.'/'.drupal_get_path('theme', $GLOBALS['theme']); ?>/images/pencil.png"><br/>Cadastros</a>
         <a class="menu-button" title="Cadastre os dados dos clientes nesta parte do sistema" href="<?php echo $base_path; ?>processos"><img class="icon" src="<?php echo $base_path.'/'.drupal_get_path('theme', $GLOBALS['theme']); ?>/images/paper.png"><br/>Processos</a>
-        <a class="menu-button" title="Cadastre os dados dos clientes nesta parte do sistema" href="<?php echo $base_path; ?>"><img class="icon" src="<?php echo $base_path.'/'.drupal_get_path('theme', $GLOBALS['theme']); ?>/images/search.png"><br/>Pesquisar</a>
+        <a class="menu-button" title="Cadastre os dados dos clientes nesta parte do sistema" href="<?php echo $base_path; ?>listagem"><img class="icon" src="<?php echo $base_path.'/'.drupal_get_path('theme', $GLOBALS['theme']); ?>/images/search.png"><br/>Pesquisar</a>
     <?php
       ////  imprimir menu da pagina inicial
-      $menu = menu_navigation_links('menu-selecao-de-campos');
-      print theme('links',array('links' => $menu)); 
+      //$menu = menu_navigation_links('menu-selecao-de-campos');
+      //print theme('links',array('links' => $menu)); 
     ?> 	  
-      <?php  if ($page['content_top']): ?><div id="content_top"><?php print render($page['content_top']); ?></div><?php endif; ?>
+      <?php /* if ($page['content_top']): ?><div id="content_top"><?php print render($page['content_top']); ?></div><?php endif; ?>
       <?php print render($title_prefix); ?>
       <?php if ($title): ?><h1 class="page-title"><?php print $title; ?></h1><?php endif; ?>
       <?php print render($title_suffix); ?>
       <?php if (!empty($tabs['#primary'])): ?><div class="tabs-wrapper clearfix"><?php print render($tabs); ?></div><?php endif; ?>
       <?php print render($page['help']); ?>
       <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
-      <?php print render($page['content']); ?>
+      <?php print render($page['content']);*/ ?>
     
 
     <!-- navigation START -->
